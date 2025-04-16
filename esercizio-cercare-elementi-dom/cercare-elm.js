e776cf1cf4443eaa83d4e7e753e03ccfa38b1b08
Silvia Baffi <sil.baffi@gmail.com>
2025-04-16 08:43:01 +0200

    Aggiunta esercizi DOM

---
+++ esercizio-cercare-elementi-dom/cercare-elm.js
@@ -0,0 +1,7 @@
+// Cerca tutti gli elementi con classe "paragrafo".
+// Cerca l' elemento con id "elemento-3"
+// Cerca tutti gli elementi h2 della pagina
+
+console.log(document.getElementsByClassName("paragrafo"))
+console.log(document.getElementById("elemento-3"))
+console.log(document.querySelectorAll("h2"))
